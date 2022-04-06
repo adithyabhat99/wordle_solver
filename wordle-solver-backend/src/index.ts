49b9d33cfb5aeee4e68929583bac821001782abe
Adithya Bhat <adithyabhat@Adithyas-MacBook-Air.local>
2022-04-07 00:31:03 +0530

    cleaned data

--- wordle-solver-backend/src/index.ts
+++ wordle-solver-backend/src/index.ts
@@ -1,10 +1,12 @@
 import express, {Request, Response} from "express";
+import words from "./data/words";
+
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
+  res.send(`Hey there, we have ${words.length} words which have 5 letters!`);
 });
 
 // start the Express server
